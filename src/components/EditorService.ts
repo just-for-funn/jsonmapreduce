893d99fca4bbebf61900f4e1193b6dad0ffa0262
davut.ozcan <davut@hostfully.com>
2022-10-20 23:39:49 +0300

    initial version working

--- src/components/EditorService.ts
+++ src/components/EditorService.ts
@@ -1,4 +1,4 @@
-import {Subject} from "rxjs";
+import {debounce, debounceTime, Subject, throttle} from "rxjs";
 
 function dynamicRun(input: any, body: any) {
 
@@ -16,25 +16,43 @@ function dynamicRun(input: any, body: any) {
 
 class EditorService {
     private source: string = "";
+    private body: string = "";
 
     public result$ = new Subject();
-    private body: any;
+    private triggerSubject = new Subject<{source:string, body:string}>();
+
+    constructor() {
+        this.triggerSubject.pipe(debounceTime( 250))
+            .subscribe( o=>{
+                this.evaluateResult(o.source,o.body);
+            });
+    }
 
     setSource(value: string) {
-        try{
-            this.source = value;
-            let result = dynamicRun(value, this.body);
-            console.log("result",result);
-            this.result$.next(JSON.stringify(result,null,"\t"));
-        }catch (e){
-            console.error(e);
-        }
-
+        this.source = value;
+        this.triggerSubject.next({
+            source: this.source,
+            body: this.body
+        });
     }
 
 
+    private evaluateResult(source: string, body: string) {
+        try {
+            let result = dynamicRun(source, body);
+            console.log("result", result);
+            this.result$.next(JSON.stringify(result, null, "\t"));
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     setBody(value: any) {
         this.body = value;
+        this.triggerSubject.next({
+            source: this.source,
+            body: this.body
+        });
     }
 };
 
