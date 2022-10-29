ffccddcd91e6ecfcdb7cbf20aed8abf78eab89d2
davut.ozcan <davut@hostfully.com>
2022-10-29 23:32:40 +0300

    working version 1

--- src/components/EditorWrapper.tsx
+++ src/components/EditorWrapper.tsx
@@ -1,12 +1,15 @@
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 import {useRef} from "react";
 import editorService from "./EditorService";
+import {mapperSample} from "./samples";
 
 export const EditorWrapper = () => {
     const editorRef = useRef<any>(null);
 
     function handleEditorDidMount(editor: any, monaco: any) {
         editorRef.current = editor;
+        editorRef.current.setValue(mapperSample);
+        editorService.setBody(mapperSample);
     }
 
     function handleEditorChange(value: any, event:any) {
--- src/components/SourceView.tsx
+++ src/components/SourceView.tsx
@@ -1,12 +1,16 @@
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 import Editor from "@monaco-editor/react";
 import editorService from "./EditorService";
+import {sourceSample} from "./samples";
 
 export const SourceView = () => {
     const editorRef = useRef<any>(null);
 
     function handleEditorDidMount(editor: any, monaco: any) {
         editorRef.current = editor;
+        var str = JSON.stringify(sourceSample, null,4);
+        editorRef.current.setValue(str);
+        editorService.setSource(str);
     }
 
     function handleEditorChange(value: any, event:any) {
